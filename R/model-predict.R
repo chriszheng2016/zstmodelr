09a68bea9df9ac038c8f8711b5648355e716eb7d
chriszheng2016 <chriszheng@vip.sina.com.cn>
2018-03-17 13:36:03 +0800

    ENH: new files

---
+++ R/model-predict.R
@@ -0,0 +1,19 @@
+
+
+model_fit <- function(){
+}
+
+model_dignose <- function() {
+}
+
+model_predict_factors_cov <- function() {
+}
+
+model_predict_returns_cov <- function() {
+}
+
+model_predict_factors_value <- function() {
+}
+
+model_predict_returns <- function() {
+}
