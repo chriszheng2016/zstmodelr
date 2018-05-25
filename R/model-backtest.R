b07d43f62071b8a5d615e6a0ccee0453f2a492bb
chriszheng2016 <chriszheng@vip.sina.com.cn>
2018-05-25 14:12:07 +0800

    ENH: inital commit for functions of model-backtest

---
+++ R/model-backtest.R
@@ -0,0 +1,15 @@
+
+
+# filter stock data by appling qualified_lists
+filter_qualified_stocks <- function( ds_stocks_data, qualified_lists ) {
+
+
+
+}
+
+# get qulified stocks of each period by applying filter stanard
+build_qualified_lists <- function() {
+
+
+
+}
