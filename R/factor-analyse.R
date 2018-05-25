b2b1a71bd5b4c542cde0dd1a4d5915523df7af34
chriszheng2016 <chriszheng@vip.sina.com.cn>
2018-05-25 14:10:06 +0800

    ENH: initial commit for functions of factor analysis

---
+++ R/factor-analyse.R
@@ -0,0 +1,25 @@
+
+# Compute factor stability coefficient
+factor_stabilitity <- function(ds_factors, factors_list = NULL) {
+  # Validate params
+}
+
+# Compute factor collinearity
+factor_collinearity <- function(ds_factors, factors_list = NULL) {
+
+  # Validate params
+
+}
+
+# Compute factor correlation
+factor_correlation <- function(ds_factors, factors_list = NULL){
+
+  # Validate params
+  assertive::assert_is_not_null(ds_factors)
+
+  fields <- parse_factor_fields(ds_factors, factors_list)
+
+
+
+
+}
