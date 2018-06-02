840177b4961ee98b62fd785550603601e7bda8e0
chriszheng2016 <chriszheng@vip.sina.com.cn>
2018-06-02 21:02:59 +0800

    BUG: fix bugs in prodcuting batch reports

--- Research/model-factor-choice/build-factor-test-report.R
+++ Research/model-factor-choice/build-factor-test-report.R
@@ -2,7 +2,9 @@ library(zstmodelr)
 
 
 # Build factors test report
-factor_test_report <- function(factors_list,
+factor_test_report <- function(
+         factors_list,
+         factors_group = NULL,
          backtest_start_date = "2016-01-01",
          backtest_end_date  =  "2017-12-31",
          report_type = c("batch", "single"),
@@ -40,49 +42,45 @@ factor_test_report <- function(factors_list,
     build_report(report_template = report_template,
                  report_params = params,
                  output_format = output_format,
-                 output_sn = NULL,
+                 output_sn = factors_group,
                  output_dir = output_dir)
    }
 }
 
 
 # Build report...
+main <- function() {
 
-# 读取因子信息
-stock_db <- stock_db(gta_db, "GTA_SQLData")
-open_stock_db(stock_db)
-ds_factors_info <- get_factors_info(stock_db, factor_groups = "Financial Risk")
+  # 读取因子信息
+  stock_db <- stock_db(gta_db, "GTA_SQLData")
+  open_stock_db(stock_db)
+  # ds_factors_info <- get_factors_info(stock_db, factor_groups = "Financial Risk")
+  ds_factors_info <- get_factors_info(stock_db, factor_groups = NULL)
 
-factors_info_by_group <- ds_factors_info %>%
-   dplyr::group_by(factor_group) %>%
-   dplyr::summarise(factors_list = list(factor_code))
 
-for (i in seq_along(factors_info_by_group)) {
+  factors_info_by_group <- ds_factors_info %>%
+    dplyr::group_by(factor_group) %>%
+    dplyr::summarise(factors_list = list(factor_code))
 
-  factors_group <- factors_info_by_group$factor_group[i]
-  factors_list <- factors_info_by_group$factors_list[i][[1]]
+  for (i in seq_along(factors_info_by_group$factor_group)) {
 
-  msg <- sprintf("generate report for factor group of :%s ...\n", factors_group)
-  message(msg)
+    factors_group <- factors_info_by_group$factor_group[i]
+    factors_list <- factors_info_by_group$factors_list[i][[1]]
 
-  factor_test_report(factors_list,
-                     backtest_start_date = "2016-01-01",
-                     backtest_end_date  =  "2017-12-31",
-                     report_type = "batch",
-                     output_format = "html_document"
-  )
+    msg <- sprintf("generate report for factor group of :%s ...\n", factors_group)
+    message(msg)
 
+    factor_test_report(factors_list,
+                       factors_group = factors_group,
+                       backtest_start_date = "2016-01-01",
+                       backtest_end_date  =  "2017-12-31",
+                       report_type = "batch",
+                       output_format = "html_document"
+    )
+  }
 }
 
 
-# fundamental_factors_list <- c("GPM", "ROCE", "PE", "PB", "CUR", "QR")
-# fundamental_factors_list <- c("PE")
-
-
-# fundamental_factors_list <- factor_test_report(fundamental_factors_list,
-#                    backtest_start_date = "2016-01-01",
-#                    backtest_end_date  =  "2017-12-31",
-#                    report_type = "single",
-#                    output_format = "rticles::ctex")
+main()
 
 
