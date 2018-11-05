057fabed9dd2451c575f3b2be82482aa7f62f28e
chriszheng2016 <chriszheng@vip.sina.com.cn>
2018-11-05 19:07:39 +0800

    ENH: add intial data-mgt tool
        Build inital data-mgt script which utilize data-manage functions
        for stock-db management

---
+++ tools/data-manage/data-manager.R
@@ -0,0 +1,32 @@
+library(zstmodelr)
+
+
+# Conduct data management
+main <- function() {
+
+  # Conect to target stock db
+  # stock_db <- stock_db(gta_db, "GTA_SQLData_TEST")
+  stock_db <- stock_db(gta_db, "GTA_SQLData")
+  open_stock_db(stock_db)
+
+
+  # Update all tables
+  update_db(stock_db,
+            retry_log = NULL,
+            log_dir = "Z:/Stock/GTA/SQL_Data/Data/Source/log")
+
+  # Update only faild tables recored in th log file
+  # update_db(stock_db,
+  #           retry_log = "update_log_GTA_SQLDATA(current).csv",
+  #           log_dir = "Z:/Stock/GTA/SQL_Data/Data/Source/log")
+
+
+  # Close stock db
+  close_stock_db(stock_db)
+
+}
+
+
+main()
+
+
