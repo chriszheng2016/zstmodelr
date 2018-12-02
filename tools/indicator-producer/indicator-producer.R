1376f0b1d851971de3215a1e52734d43a4c85444
chriszheng2016 <chriszheng@vip.sina.com.cn>
2018-12-02 21:17:20 +0800

    BUG: fix problems in indicator-producer
    fix bugs in indicator-producer and gta_profile.xlsx

--- tools/indicator-producer/indicator-producer.R
+++ tools/indicator-producer/indicator-producer.R
@@ -17,45 +17,49 @@ produce_indictors <- function() {
 
   # produce indicators batchly
   for (i in seq_len(NROW(ds_indicator_defs))) {
-
-    indicator_def <- ds_indicator_defs[i,]
+    indicator_def <- ds_indicator_defs[i, ]
     ind_def_fun <- indicator_def$ind_def_fun[[1]]
 
     if (!is.null(ind_def_fun)) {
       # compute a indicator from vars dataset.
-      msg <- sprintf("Compute indicator: %s(%s) ...\n", indicator_def$ind_code,
-                     indicator_def$ind_name)
+      msg <- sprintf(
+        "Compute indicator: %s(%s) ...\n", indicator_def$ind_code,
+        indicator_def$ind_name
+      )
       message(msg)
       ds_indicator <- compute_indicator(ds_vars,
-                                        ind_def_fun = ind_def_fun,
-                                        date_index_field = "date",
-                                        key_fields = "stkcd",
-                                        parallel = FALSE
+        ind_def_fun = ind_def_fun,
+        date_index_field = "date",
+        key_fields = "stkcd",
+        parallel = FALSE
       )
 
       # save indicators into source
       success <- save_indicators_to_source(stock_db,
-                                           indicator_source = indicator_def$ind_source,
-                                           ts_indicators = ds_indicator)
+        indicator_source = indicator_def$ind_source,
+        ts_indicators = ds_indicator
+      )
       if (success) {
-        msg <- sprintf("Save indicator: %s(%s) in %s successfully.\n",
-                       indicator_def$ind_code,
-                       indicator_def$ind_name,
-                       indicator_def$ind_source)
+        msg <- sprintf(
+          "Save indicator: %s(%s) in %s successfully.\n",
+          indicator_def$ind_code,
+          indicator_def$ind_name,
+          indicator_def$ind_source
+        )
         message(msg)
+      } else {
+        msg <- sprintf(
+          "Fail to compute indicator: %s(%s), because ind_def_fun is NULL.\n",
+          indicator_def$ind_code,
+          indicator_def$ind_name
+        )
+        warning(msg)
       }
-    } else {
-      msg <- sprintf("Fail to compute indicator: %s(%s), because ind_def_fun is NULL.\n",
-                     indicator_def$ind_code,
-                     indicator_def$ind_name)
-      warning(msg)
     }
   }
 
-
   open_stock_db(stock_db)
-
 }
 
 # Run indicators producer
-#produce_indictors()
+# produce_indictors()
