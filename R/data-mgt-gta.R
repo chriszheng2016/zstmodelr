a05392e6009075c82cad86f9ee324b28abea660c
chriszheng2016 <chriszheng@vip.sina.com.cn>
2018-12-18 20:44:21 +0800

    ENH: add feature of proccessing importing files      Processing importing files is to support building financial report of ttm format. the processed ttm format financal report will be imported into database like other original importing files.

--- R/data-mgt-gta.R
+++ R/data-mgt-gta.R
@@ -33,13 +33,18 @@ get_datasource.gta_db <- function(stock_db) {
 
     # build specified result of datasource
     ds_datasource <- ds_datasource_files %>%
-      dplyr::mutate(input_dir = input_dir) %>%
+      dplyr::mutate(
+        input_dir = input_dir,
+        start_index = as.integer(start_index)
+      ) %>%
       dplyr::select(
         target_table = target_table,
         input_file = input_file,
         input_type = input_type,
         input_dir = input_dir,
-        start_index = start_index
+        start_index = start_index,
+        process = process,
+        process_source = process_source
       )
   }
 
@@ -83,6 +88,8 @@ clear_tables.gta_db <- function(stock_db) {
   sql_cmd <- sprintf("DBCC ShrinkDatabase(%s, 0, TRUNCATEONLY)", target_database)
   # sql_cmd <- sprintf("DBCC ShrinkFile(%s_log, 1, TRUNCATEONLY)", target_database)
   DBI::dbExecute(stock_db$connection, sql_cmd)
+
+  return(invisible(NULL))
 }
 
 # Method definition for s4 generic
@@ -116,27 +123,26 @@ update_db.gta_db <- function(stock_db,
   assertive::assert_is_character(log_file_prefix)
   assertive::assert_is_character(log_dir)
 
-  success <- TRUE
-
   # get target database info
   db_info <- DBI::dbGetInfo(stock_db$connection)
   target_database <- db_info$dbname
   assertive::assert_is_not_null(target_database)
 
   # setup log info params
-  log_file_prefix <- sprintf("%s_%s",
-                             log_file_prefix,
-                             target_database)
+  log_file_prefix <- sprintf(
+    "%s_%s",
+    log_file_prefix,
+    target_database
+  )
   log_file_path <- NULL
   ds_log <- data_source %>%
     dplyr::select(target_table, input_file) %>%
     dplyr::mutate(success = FALSE)
 
   # collect failed tables from log file
-  faild_tables <- NULL
+  failed_tables <- NULL
   finished_tables <- NULL
   if (!is.null(retry_log)) {
-
     ds_retry_log <- read_log(retry_log, log_dir = log_dir)
 
     if (!is.null(ds_retry_log)) {
@@ -145,18 +151,20 @@ update_db.gta_db <- function(stock_db,
       ds_faild_tables <- ds_retry_log %>%
         dplyr::filter(success == FALSE)
       if (nrow(ds_faild_tables) > 0) {
-        faild_tables <- ds_faild_tables$target_table
+        failed_tables <- ds_faild_tables$target_table
       }
 
       # find out finished tables
       ds_finished_tables <- ds_retry_log %>%
-          dplyr::filter(success == TRUE)
-        if (nrow(ds_finished_tables) > 0) {
-          finished_tables <- ds_finished_tables$target_table
+        dplyr::filter(success == TRUE)
+      if (nrow(ds_finished_tables) > 0) {
+        finished_tables <- ds_finished_tables$target_table
       }
     } else {
-      msg <- sprintf("Since there is no a retry log(%s), \n all tables will be updated.",
-                     retry_log)
+      msg <- sprintf(
+        "Since there is no a retry log(%s), \n all tables will be updated.",
+        retry_log
+      )
       rlang::warn(msg)
     }
   }
@@ -168,27 +176,26 @@ update_db.gta_db <- function(stock_db,
     data_source_info <- ds_data_source[i, ]
 
     # conduct update for all tables or tables recording failed in retry log
-    if (is.null(faild_tables) || data_source_info$target_table %in% faild_tables) {
+    if (is.null(failed_tables) || data_source_info$target_table %in% failed_tables) {
       # conduct update for all tables or tables not recording successful in retry log
       if (is.null(finished_tables) || !(data_source_info$target_table %in% finished_tables)) {
-
         msg <- sprintf("Import data into %s ...\n", data_source_info$target_table)
         rlang:::inform(msg)
 
-        success <- import_table(stock_db,
-                                input_file = data_source_info$input_file,
-                                input_type = data_source_info$input_type,
-                                input_dir = data_source_info$input_dir,
-                                start_index = data_source_info$start_index,
-                                target_table = data_source_info$target_table,
-                                ignore_problems = TRUE,
-                                log_dir = log_dir
+        result <- import_table(stock_db,
+          input_file = data_source_info$input_file,
+          input_type = data_source_info$input_type,
+          input_dir = data_source_info$input_dir,
+          start_index = as.integer(data_source_info$start_index),
+          target_table = data_source_info$target_table,
+          ignore_problems = TRUE,
+          log_dir = log_dir
         )
 
         need_clear_dblog <- TRUE
-        ds_log$success[i] <- success
+        ds_log$success[i] <- result
 
-        if (!success) {
+        if (result != TRUE) {
           msg <- sprintf("fail to update %s !!\n", data_source_info$target_table)
           rlang:::inform(msg)
         }
@@ -200,10 +207,10 @@ update_db.gta_db <- function(stock_db,
     }
 
     # write log for update operation
-    log_file_path <- save_log(ds_log, log_file_prefix = log_file_prefix,
-                              log_dir = log_dir)
-
-
+    log_file_path <- save_log(ds_log,
+      log_file_prefix = log_file_prefix,
+      log_dir = log_dir
+    )
   }
 
   # shrink database log if needed
@@ -225,6 +232,7 @@ update_db.gta_db <- function(stock_db,
     rlang::inform(msg)
   }
 
+  return(invisible(NULL))
 }
 # Method definition for s4 generic
 #' @describeIn update_db update tables in a database of gta_db class
@@ -233,9 +241,11 @@ setMethod(
   "update_db",
   signature(stock_db = "gta_db"),
   function(stock_db, data_source, retry_log,
-           log_file_prefix, log_dir, ...) {
-    update_db.gta_db(stock_db, data_source, retry_log,
-                     log_file_prefix, log_dir)
+             log_file_prefix, log_dir, ...) {
+    update_db.gta_db(
+      stock_db, data_source, retry_log,
+      log_file_prefix, log_dir
+    )
   }
 )
 
@@ -258,18 +268,9 @@ import_table.gta_db <- function(stock_db,
     stop("Stock db isn't connected, try to connect db again")
   }
   assertive::assert_is_not_null(input_file)
-  assertive::assert_is_a_number(start_index)
+  assertive::assert_is_integer(start_index)
   assertive::assert_all_are_greater_than_or_equal_to(start_index, 1)
   assertive::assert_is_logical(ignore_problems)
-
-  success <- TRUE
-
-  # get full path of input file
-  if (!is.null(input_dir)) {
-    input_file <- paste0(input_dir, "/", basename(input_file))
-  } else {
-    input_file <- normalizePath(input_file, winslash = "/")
-  }
 
   # get data object name
   object_name <- basename(input_file)
@@ -277,93 +278,20 @@ import_table.gta_db <- function(stock_db,
     simplify = TRUE
   )[1, 1]
 
-  # read data from raw file into dataframe
-  input_type <- match.arg(input_type)
-  # Get colnames of rawdata header
-  suppressMessages(
-    raw_data_header <- switch(input_type,
-      txt = {
-        readr::read_tsv(input_file,
-          locale = readr::locale(encoding = "CP936"),
-          n_max = 0
-        )
-      },
-      csv = {
-        readr::read_csv(input_file,
-          locale = readr::locale(encoding = "CP936"),
-          n_max = 0
-        )
-      }
-    )
+  success <- TRUE
+
+  # read raw file for importing
+  raw_data_frame <- read_import_file(input_file,
+    input_type = input_type,
+    input_dir = input_dir,
+    start_index = start_index,
+    ignore_problems = ignore_problems,
+    log_dir = log_dir
   )
-  raw_data_colnames <- names(raw_data_header)
-  if (nrow(raw_data_header) != 0) {
-    success <- FALSE
+  if (is.null(raw_data_frame)) success <- FALSE
 
-    msg <- sprintf("Errors in getting header from %s", input_file)
-    stop(msg)
-  }
 
-  # read raw data into dataframe
-  suppressMessages({
-    skip <- start_index - 1L
-
-    raw_data_frame <- switch(input_type,
-      txt = {
-        readr::read_tsv(input_file,
-          locale = readr::locale(encoding = "CP936"),
-          col_names = raw_data_colnames,
-          skip = skip,
-          guess_max = 50000
-        )
-      },
-      csv = {
-        readr::read_csv(input_file,
-          locale = readr::locale(encoding = "CP936"),
-          col_names = raw_data_colnames,
-          skip = skip,
-          guess_max = 50000
-        )
-      }
-    )
-  })
-  problems_data <- readr::problems(raw_data_frame)
-  if ((nrow(raw_data_frame) == 0) || (nrow(problems_data) != 0)) {
-    if (is.null(log_dir)) {
-      problems_file <- sprintf(
-        "%s/%s(%s).csv",
-        dirname(input_file),
-        object_name,
-        format(Sys.Date(), "%Y-%m-%d")
-      )
-    } else {
-      if (!file.exists(log_dir)) {
-        dir.create(log_dir)
-      }
-
-      problems_file <- sprintf(
-        "%s/%s(%s).csv",
-        log_dir,
-        object_name,
-        format(Sys.Date(), "%Y-%m-%d")
-      )
-    }
-
-    readr::write_excel_csv(problems_data, problems_file)
-
-    msg <- sprintf(
-      "Converting problems in %s, plese check %s for detail.",
-      input_file,
-      problems_file
-    )
-    warning(msg)
-
-    if (!ignore_problems) {
-      success <- FALSE
-    }
-  }
-
-  # transfer raw data dataframe to table in stcok database
+  # transfer raw data dataframe to table in stock database
   if (success) {
     if (is.null(target_table)) {
       target_table <- object_name
@@ -400,10 +328,12 @@ import_table.gta_db <- function(stock_db,
 
     if (success) {
       msg <- sprintf("import %s successfully", input_file)
+      rlang::inform(msg)
     } else {
       msg <- sprintf("fail to import %s, error: %s", input_file, error_msg)
+      rlang::warn(msg)
     }
-    message(msg)
+
   }
 
   return(invisible(success))
@@ -425,6 +355,183 @@ setMethod(
       input_dir, start_index,
       target_table, ignore_problems,
       log_dir
+    )
+  }
+)
+
+# Process importing files in stock database
+# Method definition for s3/s4 generic
+#' @describeIn process_files process importing files in a database of gta_db class
+#' @export
+process_files.gta_db <- function(stock_db,
+                                 data_source = get_datasource(stock_db),
+                                 retry_log = NULL,
+                                 log_file_prefix = "update_db_log",
+                                 log_dir = "./log") {
+
+  # validate params
+  stopifnot(!is.null(stock_db), inherits(stock_db, "stock_db"))
+  if (is.null(stock_db$connection)) {
+    stop("Stock db isn't connected, try to connect db again")
+  }
+  assertive::assert_is_not_null(data_source)
+  assertive::assert_is_data.frame(data_source)
+  assertive::assert_is_character(log_file_prefix)
+  assertive::assert_is_character(log_dir)
+
+
+  # filter datasource files to be process
+  data_source_process <- data_source %>%
+    dplyr::filter(!is.na(process)) %>%
+    dplyr::left_join(data_source,
+      by = c("process_source" = "input_file"),
+      suffix = c("", ".source")
+    )
+
+  # get target database info
+  db_info <- DBI::dbGetInfo(stock_db$connection)
+  target_database <- db_info$dbname
+  assertive::assert_is_not_null(target_database)
+
+  # setup log info params
+  log_file_prefix <- sprintf(
+    "%s_%s",
+    log_file_prefix,
+    target_database
+  )
+  log_file_path <- NULL
+  ds_log <- data_source_process %>%
+    dplyr::select(input_file = input_file,
+                  source_file = process_source,
+                  process) %>%
+    dplyr::mutate(success = FALSE)
+
+  # collect failed tables from log file
+  failed_files <- NULL
+  finished_files <- NULL
+  if (!is.null(retry_log)) {
+    ds_retry_log <- read_log(retry_log, log_dir = log_dir)
+
+    if (!is.null(ds_retry_log)) {
+
+      # find out failed files
+      ds_faild_files <- ds_retry_log %>%
+        dplyr::filter(success == FALSE)
+      if (nrow(ds_faild_files) > 0) {
+        failed_files <- ds_faild_files$input_file
+      }
+
+      # find out finished files
+      ds_finished_files <- ds_retry_log %>%
+        dplyr::filter(success == TRUE)
+      if (nrow(ds_finished_files) > 0) {
+        finished_files <- ds_finished_files$input_file
+      }
+    } else {
+      msg <- sprintf(
+        "Since there is no a retry log(%s), \n all input files will be processed.",
+        retry_log
+      )
+      rlang::warn(msg)
+    }
+  }
+
+  # process importing files from data source
+  ds_data_source <- data_source_process
+  for (i in seq_len(nrow(ds_data_source))) {
+    data_source_info <- ds_data_source[i, ]
+
+    # process for all input_files or input_files recording failed in retry log
+    if (is.null(failed_files) || data_source_info$input_file %in% failed_files) {
+      # processing for all input_files or input_files not recording successful in retry log
+      if (is.null(finished_files) || !(data_source_info$input_file %in% finished_files)) {
+        msg <- sprintf(
+          "\nProcess %s into %s by %s...\n",
+          data_source_info$process_source,
+          data_source_info$input_file,
+          data_source_info$process
+        )
+        rlang:::inform(msg)
+
+        # build process function
+        process_fun <- NULL
+        if (!is.na(data_source_info$process)) {
+          process <- data_source_info$process
+          switch(process,
+            "TTM" = {
+              process_fun <- purrr::partial(ttm_financial_report,
+                date_index_field = "Accper",
+                key_fields = c("Stkcd", "Typrep"),
+                parallel = TRUE
+              )
+            }
+          )
+        }
+
+        # convert file through process_fun
+        result <- convert_import_file(
+          input_file = data_source_info$process_source,
+          input_type = data_source_info$input_type.source,
+          input_dir = data_source_info$input_dir.source,
+          start_index = as.integer(data_source_info$start_index.source),
+          ignore_problems = TRUE,
+          log_dir = log_dir,
+          process_fun = process_fun,
+          output_file = data_source_info$input_file,
+          output_type = data_source_info$input_type,
+          output_dir = data_source_info$input_dir
+        )
+
+        ds_log$success[i] <- result
+
+        if (!result) {
+          msg <- sprintf(
+            "Fail to process %s into %s by %s !!\n",
+            data_source_info$process_source,
+            data_source_info$input_file,
+            data_source_info$process
+          )
+
+          rlang:::inform(msg)
+        }
+      } else {
+        ds_log$success[i] <- TRUE
+      }
+    } else {
+      ds_log$success[i] <- TRUE
+    }
+
+    # write log for update operation
+    log_file_path <- save_log(ds_log,
+      log_file_prefix = log_file_prefix,
+      log_dir = log_dir
+    )
+  }
+
+  # Notify user log file info
+  if (!is.null(log_file_path)) {
+    msg <- sprintf(
+      "\nFor more info about process files, plese check %s for detail.\n",
+      log_file_path
+    )
+    rlang::inform(msg)
+  }
+
+  return(invisible(NULL))
+
+}
+
+# Method definition for s4 generic
+#' @describeIn process_files process importing files in a database of gta_db class
+#' @export
+setMethod(
+  "process_files",
+  signature(stock_db = "gta_db"),
+  function(stock_db, data_source, retry_log,
+             log_file_prefix, log_dir, ...) {
+    process_files.gta_db(
+      stock_db, data_source, retry_log,
+      log_file_prefix, log_dir
     )
   }
 )
--- tools/data-manager/data-manager.R
+++ tools/data-manager/data-manager.R
@@ -2,6 +2,50 @@
 
 library(zstmodelr)
 
+# process stock database
+process_stock_db <- function(dsn = c("GTA_SQLData", "GTA_SQLData_TEST"),
+                            retry_error = FALSE,
+                            ...) {
+
+  # connect to target stock db
+  dsn <- match.arg(dsn)
+  stock_db <- stock_db(gta_db, dsn)
+  open_stock_db(stock_db)
+
+  db_info <- DBI::dbGetInfo(stock_db$connection)
+  target_database <- db_info$dbname
+
+  # whether to retry input_files with errors
+  retry_log_file <- NULL
+  log_file_prefix = "process_files_log"
+  if (!retry_error) {
+    # update all tables
+    msg <- sprintf("\nProcess all input_files in stock db...\n")
+    message(msg)
+  } else {
+    # update only failed input_files recored in the log file
+    retry_log_file <- sprintf("%s_%s(current).csv",
+                              log_file_prefix,
+                              target_database
+    )
+    msg <- sprintf(
+      "\nRetry to process input files with errors logged in %s...\n",
+      retry_log_file
+    )
+    message(msg)
+  }
+
+  # process input_files for importing
+  log_dir <- dir_path_db(stock_db, dir_id = "DIR_DB_DATA_LOG")
+  process_files(stock_db,
+            retry_log = retry_log_file,
+            log_dir = log_dir
+  )
+
+  # close stock db
+  close_stock_db(stock_db)
+}
+
 # Update stock database
 update_stock_db <- function(dsn = c("GTA_SQLData", "GTA_SQLData_TEST"),
                             retry_error = FALSE,
@@ -23,7 +67,7 @@ update_stock_db <- function(dsn = c("GTA_SQLData", "GTA_SQLData_TEST"),
     msg <- sprintf("\nUpdate all tables in stock db...\n")
     message(msg)
   } else {
-    # update only fail tables recored in the log file
+    # update only failed tables recored in the log file
     retry_log_file <- sprintf("%s_%s(current).csv",
                               log_file_prefix,
                               target_database
@@ -83,11 +127,14 @@ clear_stock_db <- function(dsn = c("GTA_SQLData", "GTA_SQLData_TEST"),
 
 # Main function to conduct data management
 data_manager <- function(dsn = c("GTA_SQLData", "GTA_SQLData_TEST"),
-                         fun = c("update", "clear"),
+                         fun = c("process", "update", "clear"),
                          ...) {
   dsn <- match.arg(dsn)
   fun <- match.arg(fun)
   switch(fun,
+    "process" = {
+      process_stock_db(dsn, ...)
+    },
     "update" = {
       update_stock_db(dsn, ...)
     },
@@ -102,8 +149,14 @@ data_manager <- function(dsn = c("GTA_SQLData", "GTA_SQLData_TEST"),
 # Udate all tables by default
 # data_manager(dsn = "GTA_SQLData", fun = "update")
 #
-# Update tables with loged errors
+# Update tables with logged errors
 # data_manager(dsn = "GTA_SQLData", fun = "update", retry_error = TRUE)
+#
+# Process all input_files for importing by default
+# data_manager(dsn = "GTA_SQLData", fun = "process")
+#
+# Process input_files with loged errors
+# data_manager(dsn = "GTA_SQLData", fun = "process", retry_error = TRUE)
 #
 # Clear all tables by default
 # data_manager(dsn = "GTA_SQLData", fun = "clear")
