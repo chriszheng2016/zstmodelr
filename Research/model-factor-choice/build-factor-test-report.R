1833066d3616339f32f1c06b880b1eef9329d33f
chriszheng2016 <chriszheng@vip.sina.com.cn>
2018-12-26 14:40:34 +0800

    ENH: revise factor-test-report to use indicator-build feature
         revise model-factor-choice.rmd to use new feature of indicator-build.
    revise build-factor-test-report by reducing caching temp and add testing on
    limit stocks.

--- Research/model-factor-choice/build-factor-test-report.R
+++ Research/model-factor-choice/build-factor-test-report.R
@@ -1,63 +1,106 @@
 library(zstmodelr)
 
 
-# Build factors test report
-factor_test_report <- function(
-         factors_list,
-         factors_group = NULL,
-         backtest_start_date = "2016-01-01",
-         backtest_end_date  =  "2017-12-31",
-         report_type = c("batch", "single"),
-         report_template = "research/model-factor-choice/model-factor-choice.Rmd",
-         output_format = "html_document",
-         output_dir = "output"
-         )
-{
+# build factors test report
+factor_test_report <- function(factors_list,
+                               factors_group = NULL,
+                               test_start_date = "2016-01-01",
+                               test_end_date = "2017-12-31",
+                               test_stkcds = NULL,
+                               report_type = c("batch", "single"),
+                               report_template = "model-factor-choice.Rmd",
+                               output_format = "html_document",
+                               output_dir = "output") {
 
-  params <- list( backtest_start_date = backtest_start_date,
-                  backtest_end_date = backtest_end_date,
-                  debug_mode = FALSE)
+  # validate params
+  assertive::assert_is_not_null(factors_list)
+  assertive::assert_is_character(factors_list)
+  assertive::assert_is_character(test_start_date)
+  assertive::assert_is_character(test_end_date)
+  if (!is.null(factors_group)) assertive::assert_is_character(factors_group)
+  if (!is.null(test_stkcds)) assertive::assert_is_character(test_stkcds)
+
+  # build report params
+  params <- list(
+    test_start_date = test_start_date,
+    test_end_date = test_end_date,
+    test_stkcds = test_stkcds
+  )
 
   report_type <- match.arg(report_type)
   if (report_type == "single") {
     for (factor in factors_list) {
-
-      msg <- sprintf("generate report for factor:%s ...\n", factor)
+      msg <- sprintf("..generate test report for factor:%s ...\n", factor)
       message(msg)
 
-      params <- c(list(factors_list = factor ), params)
-      build_report(report_template = report_template,
-                   report_params = params,
-                   output_format = output_format,
-                   output_sn = factor,
-                   output_dir = output_dir)
+      params <- c(list(factors_list = factor), params)
+      build_report(
+        report_template = report_template,
+        report_params = params,
+        output_format = output_format,
+        output_sn = factor,
+        output_dir = output_dir
+      )
     }
-  } else{
-
-    msg <- sprintf("generate report for factor:%s ...\n",
-                   stringr::str_c(factors_list, collapse = ","))
+  } else {
+    msg <- sprintf(
+      "..generate test report for factor:%s ...\n",
+      stringr::str_c(factors_list, collapse = ",")
+    )
     message(msg)
 
-    params = c(list(factors_list = factors_list), params)
-    build_report(report_template = report_template,
-                 report_params = params,
-                 output_format = output_format,
-                 output_sn = factors_group,
-                 output_dir = output_dir)
-   }
+    params <- c(list(factors_list = factors_list), params)
+    build_report(
+      report_template = report_template,
+      report_params = params,
+      output_format = output_format,
+      output_sn = factors_group,
+      output_dir = output_dir
+    )
+  }
 }
 
 
-# Build report...
-main <- function() {
+# build factor test reports in batch mode
+build_factor_test_reports <- function(dsn = c("GTA_SQLData"),
+                                      test_start_date = "1990-01-01",
+                                      test_end_date = as.character(Sys.Date()),
+                                      test_portfolio = NULL,
+                                      report_type = c("batch", "single"),
+                                      output_format = "html_document") {
 
-  # 读取因子信息
-  stock_db <- stock_db(gta_db, "GTA_SQLData")
+  # conect to target stock db
+  stock_db <- stock_db(gta_db, dsn)
   open_stock_db(stock_db)
-  # ds_factors_info <- get_factors_info(stock_db, factor_groups = "Financial Risk")
+  init_stock_db(stock_db)
+
+  # read test portfolio
+  test_stkcds <- "all"
+  if (!is.null(test_portfolio)) {
+    if (file.exists(test_portfolio)) {
+      ds_test_portfolio <- readr::read_csv(test_portfolio)
+      test_stkcds <- stringr::str_trim(ds_test_portfolio$stkcd)
+      msg <- sprintf(
+        "Test factors on stock portfolio(%s)",
+        paste0(test_stkcds, collapse = ",")
+      )
+      rlang::inform(msg)
+    } else {
+      msg <- sprintf(
+        "Test portfolio file(%s) dosen't exist, test factors on all stocks",
+        test_portfolio
+      )
+      rlang::warn(msg)
+    }
+  } else {
+    msg <- sprintf("Test factors on all stocks")
+    rlang::inform(msg)
+  }
+
+  # read factor info
   ds_factors_info <- get_factors_info(stock_db, factor_groups = NULL)
 
-
+  # build reports by factor_group
   factors_info_by_group <- ds_factors_info %>%
     dplyr::group_by(factor_group) %>%
     dplyr::summarise(factors_list = list(factor_code))
@@ -67,20 +110,30 @@ main <- function() {
     factors_group <- factors_info_by_group$factor_group[i]
     factors_list <- factors_info_by_group$factors_list[i][[1]]
 
-    msg <- sprintf("generate report for factor group of :%s ...\n", factors_group)
-    message(msg)
+    msg <- sprintf(
+      "\nBuild report for factor group of %s ...\n",
+      factors_group
+    )
+    rlang::inform(msg)
 
+    # build report for a group
     factor_test_report(factors_list,
-                       factors_group = factors_group,
-                       backtest_start_date = "2016-01-01",
-                       backtest_end_date  =  "2017-12-31",
-                       report_type = "batch",
-                       output_format = "html_document"
+      factors_group = factors_group,
+      test_start_date = test_start_date,
+      test_end_date = test_end_date,
+      test_stkcds = test_stkcds,
+      report_type = "batch",
+      output_format = "html_document"
     )
   }
+
+  close_stock_db(stock_db)
 }
 
-
-main()
-
-
+# Run build_factor_test_reports
+#
+# Build factor test reports on all stocks
+# build_factor_test_reports()
+#
+# Build factor test reports on test stocks
+# build_factor_test_reports(test_portfolio = "test_portfolio.csv")
