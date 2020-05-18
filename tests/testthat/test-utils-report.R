80f3c2ecf28881564b8a72f2bbadfbec5cae8240
chriszheng2016 <chriszheng@vip.sina.com.cn>
2020-05-18 21:40:31 +0800

    TST:remove warnings in test-utils-report.R
    remove unnecessary warnings and ignore log files produced by tinytex pkg

--- tests/testthat/test-utils-report.R
+++ tests/testthat/test-utils-report.R
@@ -17,11 +17,15 @@ test_that("build_report, with various arguments", {
   expect_true(file.exists("output/test_report_01.html"))
 
   # build_report with pdf_document argurments ====
-  build_report(report_template = report_template,
-               report_params = list(custom_argument = "Hello ZB"),
-               output_format = "pdf_document",
-               output_sn = "01",
-               output_dir = "output")
+  # Supress deprecated warnings from ctex pkgs
+  suppressWarnings(
+    build_report(report_template = report_template,
+                 report_params = list(custom_argument = "Hello ZB"),
+                 output_format = "pdf_document",
+                 output_sn = "01",
+                 output_dir = "output")
+  )
+
   expect_true(file.exists("output/test_report_01.pdf"))
 
   # build_report with word_document argurments ====
@@ -34,11 +38,15 @@ test_that("build_report, with various arguments", {
 
   # build_report with rticles::ctex argurments ====
   report_template <- "./data/test_report_ctex.Rmd"
-  build_report(report_template = report_template,
-               report_params = list(custom_argument = "Hello ZB"),
-               output_format = "rticles::ctex",
-               output_sn = "01",
-               output_dir = "output")
+  # Supress deprecated warnings from ctex pkgs
+  suppressWarnings(
+    build_report(report_template = report_template,
+                 report_params = list(custom_argument = "Hello ZB"),
+                 output_format = "rticles::ctex",
+                 output_sn = "01",
+                 output_dir = "output")
+  )
+
   expect_true(file.exists("output/test_report_ctex_01.pdf"))
 
 
