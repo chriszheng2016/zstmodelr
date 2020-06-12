b8bd1dad96860d92ba53a65416b385215aff4aed
chriszheng2016 <chriszheng@vip.sina.com.cn>
2020-06-12 21:52:21 +0800

    * ENH: add interactive user interface for indicator-producer.R
    * add indicator-producer-ui() for interactive operation
    * add help_usage() for help

--- tools/indicator-producer/indicator-producer.R
+++ tools/indicator-producer/indicator-producer.R
@@ -194,20 +194,171 @@ clear_indicators <- function(dsn = c("GTA_SQLData"),
 # Main function to indicator producers
 indicator_producer <- function(dsn = c("GTA_SQLData"),
                                fun = c("produce", "archive", "clear"),
+                               help = FALSE,
                                ...) {
-  dsn <- match.arg(dsn)
-  fun <- match.arg(fun)
-  switch(fun,
+  if (help) {
+    help_usage()
+  } else {
+    dsn <- match.arg(dsn)
+    fun <- match.arg(fun)
+    switch(fun,
+           "produce" = {
+             produce_indicators(dsn, ...)
+           },
+           "archive" = {
+             archive_indicators(dsn, ...)
+           },
+           "clear" = {
+             clear_indicators(dsn, ...)
+           }
+    )
+  }
+}
+# Interactive UI of main function to conduct data management
+data_manager_ui <- function(fun = indicator_producer, debug = FALSE) {
+
+  # Prompt use to choose value for a argument
+  .prompt_arg_value <- function(arg, choices, fun = NULL) {
+    # Use default value as candidate of argument value
+    # code borrowed from match.arg()
+    if (missing(fun) || is.null(fun)) {
+      fun <- sys.function(sysP <- sys.parent())
+    }
+    if (missing(choices)) {
+      formal.args <- formals(fun)
+      choices <- eval(formal.args[[as.character(substitute(arg))]],
+                      envir = sys.frame(sysP <- sys.parent())
+      )
+      if (is.logical(choices)) {
+        choices <- c(FALSE, TRUE)
+      }
+    }
+
+    # Prompt user to choose a value for argument
+    arg_name <- as.character(substitute(arg))
+    if (length(choices) > 1) {
+      cli::cli_rule(center = " * select value for {arg_name} * ")
+      arg_value <- choices[utils::menu(choices)]
+    } else {
+      arg_value <- choices[1]
+    }
+
+    if (length(arg_value) > 0) {
+      cli::cli_alert_success("selected {arg_name}: {.strong {arg_value}}.\n")
+    } else {
+      cli::cli_alert_warning("please see {.strong usage} carefully.")
+      help_usage()
+      rlang::abort("Abort without argument.\n")
+    }
+
+    # Return result as "arg = value"
+    arg_value_str <- NULL
+    if (is.character(arg_value) && (length(arg_value) > 0)) {
+      arg_value_str <- glue::glue("{arg_name} = '{arg_value}'")
+    } else {
+      arg_value_str <- glue::glue("{arg_name} = {arg_value}")
+    }
+
+    arg_value_str
+  }
+
+  # Main function
+
+  # Display function logo
+  fun_name <- as.character(substitute(fun))
+  cli::cli_rule(center = "{.emph {fun_name}}")
+
+  # Choose target data source
+  dsn_arg <- .prompt_arg_value(dsn, fun = fun)
+
+  # Choose action for db
+  action_arg <- .prompt_arg_value(action, fun = fun)
+
+  # Choose other arguments for action
+  select_action <-
+    stringr::str_extract(action_arg, pattern = "(?<=').*?(?=')")
+  switch(
+    select_action,
     "produce" = {
-      produce_indicators(dsn, ...)
+      other_args <-
+        .prompt_arg_value(arg = retry_error, fun = process_stock_db)
     },
     "archive" = {
-      archive_indicators(dsn, ...)
+      other_args <-
+        .prompt_arg_value(arg = retry_error, fun = update_stock_db)
     },
     "clear" = {
-      clear_indicators(dsn, ...)
+      other_args <- .prompt_arg_value(arg = force, fun = clear_indicators)
     }
   )
+
+  # Perform action
+  actions_template <-
+    "{substitute(fun)}({dsn_arg}, {action_arg}, {other_args})"
+  action <- rlang::parse_expr(glue::glue(actions_template))
+  cat("Action might take very long time, do you want to continue?\n")
+  cli::cli_code(format(action))
+  if (utils::menu(c("Yes", "No")) == 1) {
+    if (!debug) {
+      rlang::eval_tidy(action)
+    }
+  } else {
+    cli::cli_alert_warning("Action is aborted.")
+  }
+}
+
+# Display usage
+help_usage <- function(fun = indicator_producer) {
+  fun_name <- as.character(substitute(fun))
+  cli::cli_rule(center = "{fun_name} Help")
+  cli::cli_code(args(fun))
+
+  # usage of arguments
+  cli::cli_rule(center = "Arguments")
+  cli::cli_ul()
+  cli::cli_li("dsn: name of data source of database")
+  cli::cli_li("action: action to perform")
+  ulid <- cli::cli_ul()
+  cli::cli_li("produce: producing indicators files")
+  cli::cli_li("archive: save historical indicators files")
+  cli::cli_li("clear: clear all indicators files")
+  cli::cli_end(ulid)
+  cli::cli_li("help: diplay usage or not")
+  cli::cli_end(ulid)
+  cli::cli_li("...: arguments to action function")
+  ulid <- cli::cli_ul()
+  cli::cli_li("retry_error: use logged file to retry actions with error, valid for process and update")
+  cli::cli_li("force: clear all table without comfirmation, valid for clear")
+  cli::cli_end(ulid)
+
+  # usage of examples
+  examples <- c('
+  # Produce all indicators in parallel process(Production)
+    indicator_producer(fun = "produce", parallel = TRUE)
+  # Produce all indicators in non-parallel process(Debug)
+    indicator_producer(fun = "produce", parallel = FALSE)
+  # Produce some indicators in parallel process(Production)
+    indicator_producer(fun = "produce", indicator_codes = c("m_nop_ttm"), parallel = TRUE)
+  # Validate all indicator definition
+    indicator_producer(fun = "produce", validate_def = TRUE, parallel = FALSE)
+  # Validate some indicator definition
+    indicator_producer(fun = "produce", indicator_codes = c("m_nop_ttm"),
+                      validate_def = TRUE, parallel = FALSE)
+  # Archive all indicators files by default
+    indicator_producer(fun = "archive")
+  # Archive some indicator files by default
+    indicator_producer(fun = "archive", indicator_codes = c("m_nop_ttm"))
+  # Clear all indicators files by default
+    indicator_producer(fun = "clear")
+  # Clear all indicators files forcefully
+    indicator_producer(fun = "clear", force = TRUE)
+  # Clear some indicators files
+    indicator_producer(fun = "clear", indicator_codes = c("m_nop_ttm"))
+  ')
+
+  cli::cli_text("")
+  cli::cli_rule(center = "Examples")
+  cat(examples)
 }
 
 # Run indicators producer
