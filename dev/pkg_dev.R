547bc8b54098d5b30dbde85e7b05ec3a6c4879c8
chriszheng2016 <chriszheng@vip.sina.com.cn>
2021-05-31 14:51:43 +0800

    ENH: revise develop scripts to cheatsheet
    * Restructure scripts to be include in cheatsheet_pkg.Rmd
    
    * Use cheatsheet_pkg.Rmd to build cheatsheet

--- dev/pkg_dev.R
+++ dev/pkg_dev.R
@@ -1,19 +1,40 @@
-## Add module to dependency ----
-usethis::use_package("package")
+#' ---
+#' title: "Notes about Building R Package"
+#' date:  "`r Sys.Date()`"
+#' author: Chris Zheng
+#' email: chrizheng@vip.sina.com.cn
+#' output:
+#'   html_document:
+#'      fig_caption: yes
+#'      number_sections: yes
+#'      theme: cerulean
+#'      highlight: pygments
+#' ---
+
+#' # Build R package
+
+#+ develop_r_pkg, eval=FALSE
+
+## Add package to dependency ----
+## Add one line by package you want to add as dependency
+usethis::use_package("package")  # Use package from cran
+usethis::use_dev_package(
+  "pkgname",
+  remote = "onwer/project@branch"
+  ) # Use package in development from github
 usethis::use_tidy_description()
 
 ## Add internal datasets ----
-# If you have data in your package
+## If you have data in your package
 usethis::use_data_raw(name = "my_dataset", open = FALSE)
 
-## Style codes ----
-usethis::use_tidy_style()
 
 ## Tests ----
 ## Add one line by test you want to create
 usethis::use_test("app")
 devtools::wd("tests/testthat")
 testthat::test_file("test-app.R") # test a file
+devtools::test_file_coverage("test-app.R") # test a file coverage
 
 # Test a file which contains "skip_on_cran()/skip_on_ci()/skip_on_covr()"
 # Method A:
@@ -29,39 +50,35 @@ testthat::test_file("test-app.R")
 
 devtools::test() # test package
 
-## Document ----
-
-# Update function doc
-devtools::document()
-?fun_abc
-
-# Check spelling in doc
-devtools::document() # update all doc
-usethis::use_spell_check() # set up spelling check in doc
-devtools::spell_check() # check spelling in doc
-spelling::update_wordlist() # accept new words if need
-
-# Vignette
-usethis::use_vignette("pkgname")
+## Edit vignette
+usethis::use_vignette("pkg_name")
 devtools::build_vignettes()
-
 
 ## Check before merging ----
 
-# Test package
+## Test package
 devtools::test()
 
-# Check spelling
-devtools::document()  # Update document for checking
-usethis::use_spell_check() # Set up spelling check by creating word list
-devtools::spell_check() # Check spelling according to word list
+## Test coverage
+devtools::test_coverage()
 
-# Check doc
+## Style package
+## Styles source code according to the tidyverse style guide.
+usethis::use_tidy_style() ## It will overwrite files!
+
+## Update doc and check spelling
+devtools::document()
+usethis::use_spell_check() # Set up spelling check in doc
+devtools::spell_check()
+
+## Check doc
 devtools::check_man()
 
-# R CMD Check
-result <- devtools::check(cran = FALSE, args = c("--timings", "--no-tests"),
-                          check_dir = "check")
+## R CMD Check
+result <- devtools::check(
+  cran = FALSE, args = c("--timings", "--no-tests"),
+  check_dir = "check"
+)
 cat(result$errors)
 cat(result$warnings)
 cat(result$notes)
---
+++ dev/pkg_tools.R
@@ -0,0 +1,86 @@
+#' ---
+#' title: "Notes about Tools for operation for zstmodler"
+#' date:  "`r Sys.Date()`"
+#' author: Chris Zheng
+#' email: chrizheng@vip.sina.com.cn
+#' output:
+#'   html_document:
+#'      fig_caption: yes
+#'      number_sections: yes
+#'      theme: cerulean
+#'      highlight: pygments
+#' ---
+
+#' # Tools for operation
+
+#' When you need to update database and produce indicator periodically,
+#' here are some notes for your reference.
+#'
+
+#' ## Update stock database
+
+#' ### Prepare raw data from GTA(SQL_DATA/Data/Origin/)
+
+#' 1. Download raw data files from GTA into “Download”, please see
+#'   "Download_Files_Notes(Current)" in "SQL_Data/Data/Source/Guide/Specification/
+#'
+#' 2. Backup old raw files in "SQL_DATA/Data/Source/Origin/" into "Backup", and remove
+#'   all files except "gta_fieldname.csv".
+#'
+#' 3. Unzip new raw data file into "SQL_DATA/Data/Origin".
+#'
+
+#' ### Use "Microsoft SQL Server Management Stdio" to backup old database
+
+#' 1. Detach GTA_SQLDATA.
+#'
+#' 2. Compress "GTA_SQLDATA.mdf" and "GTA_SQLDATA_log.ldf" into a zip file.
+#'
+#' 3. Save zip file into "./../Data_Archive" like "FY2020Q1(2020-9-11)",in which
+#'  FY2020Q1 means last date of report date of data.
+#'
+
+#' ### Use data-manager.R to update/process/clean stock database.
+
+#+ data_mgt, eval=FALSE
+
+# Update stock database ----
+
+## 1.Load data-manager.R.
+source("tools/data-manager/data-manager.R")
+
+## 2.Process data before importing into database
+# Process all input_files for importing by default
+data_manager(dsn = "GTA_SQLData", action = "process")
+# Retry process input_files with logged errors
+data_manager(dsn = "GTA_SQLData", action = "process", retry_error = TRUE)
+
+## 3.Update data in database with proceed files.
+# Update all tables by default
+data_manager(dsn = "GTA_SQLData", action = "update")
+# Retry to update tables when countering errors in updating
+data_manager(dsn = "GTA_SQLData", action = "update", retry_error = TRUE)
+
+## 4.Clear data in database if needed.
+data_manager(dsn = "GTA_SQLData", action = "clear")
+
+#' ## Update indicators data
+
+#' ### Use indicator-producer.R to backup/clean/produce indicators
+
+#+ indicator_producer, eval = FALSE
+
+# Update indicators data ----
+
+## 1. Load indicator-producer.R
+source("tools/indicator-producer/indicator-producer.R")
+
+## 2.Archive all indicators files by default
+indicator_producer(action = "archive")
+
+## 3.Clear all indicators files by default
+indicator_producer(action = "clear")
+
+## 4.Produce all indicators in parallel process(Production)
+indicator_producer(action = "produce", parallel = TRUE)
+
